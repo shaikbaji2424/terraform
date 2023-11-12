d0d7556d38f896fb777b831a0c79433821b7f641
shaikbaji2424 <ubuntu@ip-172-31-65-54.ec2.internal>
2023-11-12 08:53:46 +0000

    pvtip update

--- vpc.tf
+++ vpc.tf
@@ -19,3 +19,26 @@ resource "aws_subnet" "tf-ecom-pub-sn-A" {
     Name = "ecom-pub-subnet-A"
   }
 } 
+
+# create pub subnet - AZ -B
+resource "aws_subnet" "tf-ecom-pub-sn-B" {
+  vpc_id     = aws_vpc.tf-ecom.id
+  cidr_block = "192.168.0.0/24"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "ecom-pub-subnet-B"
+  }
+}
+
+# create pvt subnet - AZ -A
+resource "aws_subnet" "tf-ecom-pub-sn-A" {
+  vpc_id     = aws_vpc.tf-ecom.id
+  cidr_block = "192.168.0.0/24"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "ecom-pvt-subnet-A"
+  }
+}
+
