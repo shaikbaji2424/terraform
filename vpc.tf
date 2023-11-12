70ccb215547bd45521e4187415097cb9e6c7a4dc
shaikbaji2424 <ubuntu@ip-172-31-65-54.ec2.internal>
2023-11-12 09:01:34 +0000

    pvtip update

--- vpc.tf
+++ vpc.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "tf-ecom" {
 # create pub subnet - AZ -A
 resource "aws_subnet" "tf-ecom-pub-sn-A" {
   vpc_id     = aws_vpc.tf-ecom.id
-  cidr_block = "192.168.0.0/24"
+  cidr_block = "192.168.0.0/23"
   availability_zone = "us-east-2a"
   map_public_ip_on_launch = "true"
   tags = {
