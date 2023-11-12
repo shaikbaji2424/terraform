a8355a91b12cb35bbc344e1b04ec7fbd7d72368b
shaikbaji2424 <ubuntu@ip-172-31-65-54.ec2.internal>
2023-11-12 08:45:13 +0000

    update public ip

--- vpc.tf
+++ vpc.tf
@@ -14,6 +14,7 @@ resource "aws_subnet" "tf-ecom-pub-sn-A" {
   vpc_id     = aws_vpc.tf-ecom.id
   cidr_block = "192.168.0.0/24"
   availability_zone = "us-east-2a"
+  map_public_ip_on_launch = "true"
   tags = {
     Name = "ecom-pub-subnet-A"
   }
